e53441b628657c5ea6adb2f4a02b56d6bbdc52e9
Kouichi Nakanishi <keizo.bookman@gmail.com>
2017-07-16 02:46:27 +0900

    Progress a little in examples

--- examples/SimpleClient.hs
+++ examples/SimpleClient.hs
@@ -1,4 +1,16 @@
 module Main where
 
+import           Network.QUIC
+
+uname = "localhost"
+uport = 8081
+
+lhost = "localhost"
+lport = 8082
+
+cfg = undefined
+
 main :: IO ()
-main = undefined
+main = do
+    mgr <- newManager cfg
+    handshake mgr
--- examples/SimpleServer.hs
+++ examples/SimpleServer.hs
@@ -1,4 +1,8 @@
 module Main where
 
+cfg = undefined
+
 main :: IO ()
-main = undefined
+main = server cfg
+  where
+    server = undefined
