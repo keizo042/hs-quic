5df3d6d77a2d101fd50bdbf6dee1c53b812fff1f
Kouichi Nakanishi <keizo.bookman@gmail.com>
2017-07-16 03:47:48 +0900

    Add Manager module

--- Network/QUIC.hs
+++ Network/QUIC.hs
@@ -1,11 +1,5 @@
 module Network.QUIC
   (
-    ManagerSetting(..),
-    Manager(..),
-    newManager,
-    closeManager,
-    withManager,
-
     Packet(..),
     Header(..),
     Frame(..),
@@ -36,31 +30,6 @@ import qualified Network.TLS           as TLS
 data ManagerSetting = ManagerSetting { managerSettingPort :: Int
                                      , managerSettingHost :: !String}
                     deriving Show
-
-
--- Manager API
--- default ManageSetting configuration paramater
-defaultManagerSetting = ManagerSetting 4043 "localhost"
-
--- socket, connection stream, key abstraction.
-data Manager = Manager { mgrConns :: [Int]
-                        ,mgrPeer  :: Int
-                        ,mgrSock  :: S.Socket
-                        -- hash from 4 tuple to socket
-                       }
-             deriving Show
-
--- newManager generate `Manager`.
-newManager :: ManagerSetting -> IO Manager
-newManager = return undefined
-
--- closeManager close `Manager`.
-closeManager :: Manager -> IO ()
-closeManager mgr = undefined
-
--- withManager take care action IO a.
-withManager :: Manager -> IO a -> IO [Frame]
-withManager = undefined
 
 -- Header
 
---
+++ Network/QUIC/Manager.hs
@@ -0,0 +1,25 @@
+module Network.QUIC.Manager
+  where
+
+-- Manager API
+-- default ManageSetting configuration paramater
+defaultManagerSetting = ManagerSetting 4043 "localhost"
+-- socket, connection stream, key abstraction.
+data Manager = Manager { mgrConns :: [Int]
+                        ,mgrPeer  :: Int
+                        ,mgrSock  :: S.Socket
+                        -- hash from 4 tuple to socket
+                       }
+             deriving Show
+
+-- newManager generate `Manager`.
+newManager :: ManagerSetting -> IO Manager
+newManager = return undefined
+
+-- closeManager close `Manager`.
+closeManager :: Manager -> IO ()
+closeManager mgr = undefined
+
+-- withManager take care action IO a.
+withManager :: Manager -> IO a -> IO [Frame]
+withManager = undefined
